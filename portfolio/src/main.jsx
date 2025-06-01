40913c0159e2d34d152e3eb14a8f0bd16be0342b
Ava <avajeano@gmail.com>
2025-06-01 12:25:58 -0400

    url base update

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/portfolio">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </StrictMode>,
