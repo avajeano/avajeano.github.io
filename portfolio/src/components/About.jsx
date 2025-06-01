cee7ddc15d4bf0b69b2e1a23f76852e017199268
Ava <avajeano@gmail.com>
2025-05-31 20:48:48 -0400

    about me section

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -1,7 +1,6 @@
 import './style sheets/About.css'
 import '../index.css'
-import leftArrow from '../assets/left-arrow.svg';  
-import rightArrow from '../assets/right-arrow.svg';
+import Arrow from '../assets/arrow.svg';  
 
 function About() {
     return (
@@ -11,36 +10,47 @@ function About() {
                 Operations planner turned software engineer with a passion for solving complex problems and building intuitive, data-driven tools. I bring a unique blend of technical skills and business insight – ideal for roles at the intersection of engineering, product, and customer success.
             </p>
 
-            <div>
+            <div className="experience-boxes">
 
-                <div className="experience-box right">
-                    <div className="content-box-right">
+                <div className="experience-box">
+                    <div className="content-box">
                         <h3>Continuous Learning</h3>
+                        <p>side projects</p>
                     </div>
                 </div>
 
-                <div className="experience-box left">
-                    <img src={leftArrow} alt="arrow left up" className="arrow-image" />
-                    <div className="content-box-left">
+                <div className="experience-box">
+                    <img src={Arrow} alt="arrow" className="arrow-image" />
+                    <div className="content-box">
                         <h3>Software Engineering Certificate</h3>
+                        <p>2023 - 2024</p>
                     </div>
                 </div>
 
-                <div className="experience-box right">
-                    <img src={rightArrow} alt="arrow right up" className="arrow-image" />
-                    <div className="content-box-right">
+                <div className="experience-box">
+                    <img src={Arrow} alt="arrow" className="arrow-image" />
+                    <div className="content-box">
                         <h3>Operations Planner</h3>
+                        <p>2021 - present</p>
                     </div>
                 </div>
 
-                <div className="experience-box left">
-                    <img src={leftArrow} alt="arrow left up" className="arrow-image" />
-                    <div className="content-box-left">
+                <div className="experience-box">
+                    <img src={Arrow} alt="arrow" className="arrow-image" />
+                    <div className="content-box">
+                        <h3>freeCodeCamp</h3>
+                        <p>dabled in the basics during covid</p>
+                    </div>
+                </div>
+
+                <div className="experience-box">
+                    <img src={Arrow} alt="arrow" className="arrow-image" />
+                    <div className="content-box">
                         <h3>Fashion Institute of Technology</h3>
+                        <p>b.s. in business</p>
                     </div>
-
                 </div>
-                
+
             </div>
         </section>
     );
