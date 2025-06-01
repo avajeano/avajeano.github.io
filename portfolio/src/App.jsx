8a5eecbc792b0ca237be3c9a9b1243efa2656bc9
Ava <avajeano@gmail.com>
2025-06-01 11:36:36 -0400

    App.jsx update

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
 
 function App() {
   return (
