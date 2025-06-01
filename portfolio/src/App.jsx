83261c17c54c1892036b336511a747c66f1c8e97
Ava <avajeano@gmail.com>
2025-06-01 15:25:46 -0400

    bug fix

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,10 +3,28 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+function ScrollRedirectHandler() {
+  const location = useLocation();
+
+  // handle redirect
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const redirect = params.get("redirect");
+    if (redirect) {
+      window.history.replaceState(null, "", redirect);
+    }
+  }, [location]);
+
+  return null;
+}
 
 function App() {
   return (
     <>
+      <ScrollRedirectHandler />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
