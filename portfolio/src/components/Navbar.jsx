e5a3acae30f4576ff6fde976d18e3f5dc18283f2
Ava <avajeano@gmail.com>
2025-06-21 11:10:53 -0400

    resume link

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ function Navbar() {
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="https://drive.google.com/file/d/1KkOrfXj6Ai_DLdwmelF9sPfuzuB01SGP/view?usp=drive_link">Resume</Link></li>
       </ul>
     </nav>
   );
